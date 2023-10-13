eb66a0e8d987fe24b557a80960a47811607c3735
aboufazel <a.ab2521@gmail.com>
2023-10-13 13:31:12 +0330

    feat : copy right add done

--- src/components/sideMenu/sideMenu.js
+++ src/components/sideMenu/sideMenu.js
@@ -12,6 +12,7 @@ const SideMenu = ({manageOpenAndClose}) => {
                    <SideMenuCard manageOpenAndClose={manageOpenAndClose} index={index} data={items}/>
                ))
            }
+           {manageOpenAndClose ? "" : <p className={"absolute left-[48px] transition-all duration-1000 bottom-5 text-center text-text-color-2 text-[12px]"}>{" Develop With Aboufazel 2023 "}</p>}
        </div>
     )
 }
