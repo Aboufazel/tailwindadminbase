b50ed5b5c038432ffedbbf5e70d6c769b684a35a
aboufazel <a.ab2521@gmail.com>
2023-11-23 10:56:05 +0330

    ref : account type api refactor done

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,6 +1,5 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import AuthLayouts from "../layouts/authLayouts";
-import SignUp from "../layouts/auth/signUp";
 import AuthProvider from "../services/authProvider";
 import Admin from "../layouts/main/admin";
 import MainLayouts from "../layouts/mainLayouts";
